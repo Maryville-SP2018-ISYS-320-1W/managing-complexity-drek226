8760d795431de7e2e21bc206bb82eb9d2ea29c8e
drek226 <37354452+drek226@users.noreply.github.com>
2018-03-25 20:43:22 -0500

    attempt 3

--- src/P1_OutputLoopResult.java
+++ src/P1_OutputLoopResult.java
@@ -1,12 +1,14 @@
 /*
   	ISYS 320
-  	Name(s):
-  	Date: 
+  	Name(s): Derek Rieger
+  	Date: 3/25/18
 */
 
 
 
 public class P1_OutputLoopResult {
+
+	
 
 	public static void main(String[] args) {
 		printSumOfSquaresTo5();
@@ -16,9 +18,9 @@ public class P1_OutputLoopResult {
 		for( int i = 1; i <= 5; i++ ) {
 			int finalResult = 0;
 			finalResult = finalResult + i * i;
+			System.out.println( "Final result is: " + i);
 		}
 		
-		System.out.println( "Final result is: " + finalResult );
-	}
 
+	}
 }
